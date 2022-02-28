9c30fc3e136a21690b6d98cfe52314b920753bdf
Chimpslayer <94501127+Chimpslayer@users.noreply.github.com>
2022-02-28 10:42:48 -0500

    Create 8ballIfElse.js
    Magic 8 ball code with If/Else statements.

---
+++ 8ballIfElse.js
@@ -0,0 +1,35 @@
+const userName ='Wally'
+if (userName) {
+    console.log(`Hello, ${userName}!`)
+} else { console.log('Hello!');
+}
+
+const userQuestion = 'Am I the strongest?';
+if (userName) {
+    console.log(`${userName} asked "${userQuestion}"`)
+} else if (!userName) {
+    console.log(`You asked "${userQuestion}"`)
+}
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = ' ';
+
+if (randomNumber === 0) {
+    eightBall = 'It is certain.';
+} else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so.';
+} else if (randomNumber === 2) {
+    eightBall = 'Relpy hazy, try again.';
+} else if (randomNumber === 3) {
+    eightBall = 'Cannot predict now.';
+} else if (randomNumber === 4) {
+    eightBall = 'Do not count on it.';
+} else if (randomNumber == 5) {
+    eightBall = 'My sources say no.';
+} else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good.';
+} else if (randomNumber === 7) {
+    eightBall = 'Signs point to yes.'
+}
+console.log(eightBall)
